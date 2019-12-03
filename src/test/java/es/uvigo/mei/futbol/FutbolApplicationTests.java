0b90f9fdcc18cf61433b3de8eb468a4555d5a78f
Iván Martínez <i.martinezestevez@gmail.com>
2019-12-03 16:22:29 +0100

    Added project settings

---
+++ src/test/java/es/uvigo/mei/futbol/FutbolApplicationTests.java
@@ -0,0 +1,13 @@
+package es.uvigo.mei.futbol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FutbolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
